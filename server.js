ab1a1bd1972fff5c27de1f5ade88a46c0723ae08
americanogeorgie-cyber <americanogeorgie@gmail.com>
2026-01-22 03:09:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/lista.m3u", (req, res) => {
-  res.set("Content-Type", "application/vnd.apple.mpegurl");
-
-  res.send(`#EXTM3U
-
-#EXTINF:-1, Meu Filme
-#EXTVLCOPT:sub-file=https://drive.google.com/uc?export=download&id=1qUbG4CjRFdsW4xTnwSsmf6H0djndPhji
-https://drive.google.com/uc?export=download&id=1-5qezhQBIHxkjBBRB0wjDSF-ZOFuGTsZ
-`);
+app.get("/", (req, res) => {
+  res.send("IPTV API online 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("IPTV rodando");
+app.get("/playlist.m3u", (req, res) => {
+  res.setHeader("Content-Type", "audio/x-mpegurl");
+
+  const playlist = `#EXTM3U
+
+#EXTINF:-1, Meu Filme
+https://drive.google.com/uc?export=download&id=1-5qezhQBIHxkjBBRB0wjDSF-ZOFuGTsZ
+
+#EXTINF:-1, Legenda
+https://drive.google.com/uc?export=download&id=1qUbG4CjRFdsW4xTnwSsmf6H0djndPhji
+`;
+
+  res.send(playlist);
+});
+
+app.listen(port, () => {
+  console.log("IPTV rodando na porta " + port);
 });
